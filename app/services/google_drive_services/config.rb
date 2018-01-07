1923634e372455ba38cebe8e2170aaffc688d2ad
pariente <parientethomas@gmail.com>
2018-01-07 21:07:52 +0100

    updated with french clients spreadsheet key

--- app/services/google_drive_services/config.rb
+++ app/services/google_drive_services/config.rb
@@ -4,7 +4,7 @@ module GoogleDriveServices
       {
         orders_global: "1nhawjf0iA13xhgAFphrLhFciRRyMPV69afT91lLtIbg",
         orders_us: "1Mg4kfLsYz5xP3cpysfI_JWELCFK8jfrJ6hFw4ilOVq4",
-        users: "1vVcSz_TA2_-LIRMiP12NXAwPH3lkEVsY_n54YnOxWHg"
+        users: "1no9_hV0mVtKw-ITUtKkimQo5ZE9PQ2XyYs4qNBbXIgE"
       }
   end
 end
