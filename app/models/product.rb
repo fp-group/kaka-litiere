93d53ec72945cdcdb43776e9976dbec27ed1c4c4
Nicolas Filzi <nfilzi.webservices@gmail.com>
2018-01-07 22:15:42 +0200

    Fix things

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,7 @@ class Product < ApplicationRecord
   serialize :features
   has_many :order_details
   has_many :orders, through: :order_details
-  
-  
+
   validates :designation, :unit_price, presence: true
 
   # FIXME: put that in a decorator, but draper is broken
