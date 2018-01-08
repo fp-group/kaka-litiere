5445016323e3c1fc9572b923eb67ace5cc5cb25b
Nicolas Filzi <nfilzi.webservices@gmail.com>
2018-01-08 10:44:45 +0200

    Replace host with correct heroku app url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_key: ENV['KAKA_LITIERE_POSTMARK_API_KEY'] }
-  config.action_mailer.default_url_options = { host: "https://kaka-litter.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "https://kaka-litiere.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
